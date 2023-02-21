aadebaa16d9e44e22de33b108997231a8d680b53
Muhammad Usama <muhammadusama30289@gmail.com>
2023-02-21 16:58:13 +0500

    Update main.js

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -86,6 +86,7 @@
 // * 82. The While Loop
 // * 83. Do...While Statements
 // * 84. The break Keyword
+// * 85. PROJECT: Whale Talk
 
 // !=============================================
 // !=============================================
@@ -1963,4 +1964,34 @@ console.log("And if you don't know, now you know.");
 // !=============================================
 // !=============================================
 
-// * 84. The break Keyword
+// * 85. PROJECT: Whale Talk
+
+const input = "turpentine and turtles";
+
+const vowels = ["a", "e", "i", "o", "u"];
+
+const resultArray = [];
+
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+  // console.log(`inputIndex is ${inputIndex}`);
+  if (input[inputIndex] === "e") {
+    resultArray.push(input[inputIndex]);
+  }
+  if (input[inputIndex] === "u") {
+    resultArray.push(input[inputIndex]);
+  }
+  for (let vowelIndex = 0; vowelIndex < vowels.length; vowelIndex++) {
+    //console.log(`vowelIndex is ${vowelIndex}.`);
+    if (input[inputIndex] === vowels[vowelIndex]) {
+      //console.log(input[inputIndex]);
+      resultArray.push(input[inputIndex]);
+    }
+  }
+}
+
+//console.log(resultArray);
+
+const resultString = resultArray.join("").toUpperCase();
+console.log(resultString);
+
+//output: UUEEIEEAUUEE
