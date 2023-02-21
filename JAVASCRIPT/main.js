c306e67ecf58117d150538f7201e81c534930bb0
Muhammad Usama <muhammadusama30289@gmail.com>
2023-02-21 22:38:42 +0500

    Update main.js

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -101,6 +101,9 @@
 // * 97. Arrow Functions and this
 // * 98. Privacy
 // * 99. Getters
+// * 101. Factory Functions
+// * 102. Property Value Shorthand
+// * 103. Destructured Assignment
 
 // !=============================================
 // !=============================================
@@ -2375,3 +2378,93 @@ const robot = {
 
 robot.numOfSensors = 100;
 console.log(robot.numOfSensors);
+
+// !=============================================
+// !=============================================
+
+// * 101. Factory Functions
+
+const robotFactory = (model, mobile) => {
+  return {
+    model: model,
+    mobile: mobile,
+    beep() {
+      console.log("Beep Boop");
+    },
+  };
+};
+
+const tinCan = robotFactory("P-500", true);
+tinCan.beep();
+
+// !=============================================
+// !=============================================
+
+// * 102. Property Value Shorthand
+
+const robotFactory = (model, mobile) => {
+  return {
+    model,
+    mobile,
+    beep() {
+      console.log("Beep Boop");
+    },
+  };
+};
+
+// To check that the property value shorthand technique worked:
+const newRobot = robotFactory("P-501", false);
+console.log(newRobot.model);
+console.log(newRobot.mobile);
+
+// !=============================================
+// !=============================================
+
+// * 103. Destructured Assignment
+
+const robot = {
+  model: "1E78V2",
+  energyLevel: 100,
+  functionality: {
+    beep() {
+      console.log("Beep Boop");
+    },
+    fireLaser() {
+      console.log("Pew Pew");
+    },
+  },
+};
+
+const { functionality } = robot;
+
+functionality.beep();
+
+// !=============================================
+// !=============================================
+
+// * 104. Built-in Object Methods
+
+const robot = {
+  model: "SAL-1000",
+  mobile: true,
+  sentient: false,
+  armor: "Steel-plated",
+  energyLevel: 75,
+};
+
+// What is missing in the following method call?
+const robotKeys = Object.keys(robot);
+
+console.log(robotKeys);
+
+// Declare robotEntries below this line:
+const robotEntries = Object.entries(robot);
+console.log(robotEntries);
+
+// Declare newRobot below this line:
+const newRobot = Object.assign(
+  { laserBlaster: true, voiceRecognition: true },
+  robot
+);
+
+console.log(newRobot);
