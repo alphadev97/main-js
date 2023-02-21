72ad5d1d46fb03f7e854267ff0f8971d16d162a7
Muhammad Usama <muhammadusama30289@gmail.com>
2023-02-21 19:17:15 +0500

    Update main.js

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -2190,3 +2190,29 @@ let firstPassenger = spaceship.passengers[0];
 // !=============================================
 
 // * 94. Pass By Reference
+
+let spaceship = {
+  "Fuel Type": "Turbo Fuel",
+  homePlanet: "Earth",
+};
+
+// Write your code below
+
+let greenEnergy = (obj) => {
+  obj["Fuel Type"] = "avocado oil";
+};
+
+let remotelyDisable = (obj) => {
+  obj.disabled = true;
+};
+
+greenEnergy(spaceship);
+
+remotelyDisable(spaceship);
+
+console.log(spaceship);
+
+//output:
+// { 'Fuel Type': 'avocado oil',
+//   homePlanet: 'Earth',
+//   disabled: true }
