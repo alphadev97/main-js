cb6fd0b261f667abbac9578dba26543f922a1803
Muhammad Usama <muhammadusama30289@gmail.com>
2023-02-21 18:42:16 +0500

    Update main.js

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -90,6 +90,11 @@
 // * 86. Exercise: Spelling Wizard
 // * 87. Exercise: Pokemon Catcher
 // * 88. Creating Object Literals
+// * 89. Accessing Properties
+// * 90. Bracket Notation
+// * 91. Property Assignment
+// * 92. Methods
+// * 93. Nested Objects
 
 // !=============================================
 // !=============================================
@@ -2046,3 +2051,136 @@ let fasterShip = {
   "Fuel Type": "Turbo Fuel",
   color: "silver",
 };
+
+// !=============================================
+// !=============================================
+
+// * 89. Accessing Properties
+
+let spaceship = {
+  homePlanet: "Earth",
+  color: "silver",
+  "Fuel Type": "Turbo Fuel",
+  numCrew: 5,
+  flightPath: ["Venus", "Mars", "Saturn"],
+};
+
+// Write your code below
+
+let crewCount = spaceship.numCrew;
+let planetArray = spaceship.flightPath;
+
+// !=============================================
+// !=============================================
+
+// * 90. Bracket Notation
+
+let spaceship = {
+  "Fuel Type": "Turbo Fuel",
+  "Active Mission": true,
+  homePlanet: "Earth",
+  numCrew: 5,
+};
+
+let propName = "Active Mission";
+
+// Write your code below
+let isActive = spaceship["Active Mission"];
+
+console.log(spaceship[propName]);
+
+// !=============================================
+// !=============================================
+
+// * 91. Property Assignment
+
+let spaceship = {
+  "Fuel Type": "Turbo Fuel",
+  homePlanet: "Earth",
+  color: "silver",
+  "Secret Mission": "Discover life outside of Earth.",
+};
+
+// Write your code below
+
+spaceship.color = "glorious gold";
+spaceship.numEngines = 9;
+delete spaceship["Secret Mission"];
+
+console.log(spaceship);
+
+// output:
+// { 'Fuel Type': 'Turbo Fuel',
+//   homePlanet: 'Earth',
+//   color: 'glorious gold',
+//   numEngines: 9 }
+
+// !=============================================
+// !=============================================
+
+// * 92. Methods
+
+let retreatMessage =
+  "We no longer wish to conquer your planet. It is full of dogs, which we do not care for.";
+
+// Write your code below
+
+let alienShip = {
+  retreat() {
+    console.log(retreatMessage);
+  },
+  takeOff() {
+    console.log("Spim... Borp... Glix... Blastoff!");
+  },
+};
+
+alienShip.retreat();
+
+alienShip.takeOff();
+
+//output:
+// We no longer wish to conquer your planet. It is full of dogs, which we do not care for.
+// Spim... Borp... Glix... Blastoff!
+
+// !=============================================
+// !=============================================
+
+// * 93. Nested Objects
+
+let spaceship = {
+  passengers: [{ name: "Wolf" }],
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032,
+  },
+  crew: {
+    captain: {
+      name: "Sandra",
+      degree: "Computer Engineering",
+      encourageTeam() {
+        console.log("We got this!");
+      },
+      "favorite foods": ["cookies", "cakes", "candy", "spinach"],
+    },
+  },
+  engine: {
+    model: "Nimbus2000",
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD",
+    },
+    "back-up": {
+      battery: "Lithium",
+      terabytes: 50,
+    },
+  },
+};
+
+let capFave = spaceship.crew.captain["favorite foods"][0];
+
+passengers: [{ name: "Wolf" }];
+
+let firstPassenger = spaceship.passengers[0];
