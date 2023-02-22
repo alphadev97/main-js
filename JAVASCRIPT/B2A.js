1eb2fc9e694d32a46d4d5f2edb9dcb7833210cda
Muhammad Usama <muhammadusama30289@gmail.com>
2023-02-22 19:07:36 +0500

    Update B2A.js

--- JAVASCRIPT/B2A.js
+++ JAVASCRIPT/B2A.js
@@ -1,1 +1,28 @@
-// >>>>>>>>>>>>>>>>> ECMAScript 2016 (ES6) <<<<<<<<<<<<<<<<<<< //
+// * >>>>>>>>>>>>>>>>> JAVASCRIPT COURSE (Beginner to Advance) <<<<<<<<<<<<<<<<<<< //
+
+// ! >>>>>>>>>>>>>>>>> TIMESTAMP:  <<<<<<<<<<<<<<<<<<< //
+
+// >>>>>>>>>>>>>>>>> TABLE OF CONTENT <<<<<<<<<<<<<<<<<<< //
+
+// >> 1. VARIABLES
+
+// declaring a variable in JS
+
+Var x = '';
+
+var cheese = 'yummy';
+
+var age = 25;
+
+// variables are case sensitive
+
+var 1cat = ''; //wrong                      var cat = 'this cat'; //This is true
+car $cat = ''; //wrong                      var CAT = 'that cat'; //This is true
+var my-cat = ''; //wrong                    var Cat = 'another cat'; //Tis is true
+var my cat = ''; //wrong                    var myCat = 'camel case name'; //This is true
+var new = ''; //wrong (new is a keyword in JS)
+
+// reserved words in JS (https://www.w3schools.com/js/js_reserved.asp);
+
+// Practice in Google Chrome console
+
